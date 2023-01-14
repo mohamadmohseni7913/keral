ff80a9faf6fd89cf5359f6b9d515d8a65301d3d2
mohamad mohseni <83003677+mohamadmohseni7913@users.noreply.github.com>
2023-01-14 22:21:35 +0330

    first

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainIndex from "./MainIndex/MainIndex";
+
+const App = () => {
+    return (
+        <div>
+            <MainIndex/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/component/MainIndex/Services/Services.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className="services">
+            <div className="services-Section">
+                <i className="fa fa-helicopter"></i>
+                <h2>customer service</h2>
+
+            </div>
+            <div className="services-Section">
+                <i className="fa fa-helicopter"></i>
+                <h2>customer service</h2>
+
+            </div>
+            <div className="services-Section">
+                <i className="fa fa-helicopter"></i>
+                <h2>customer service</h2>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Services;
